0a01842c296435b81a5756e1ab40f03c640da10c
smiface <smiface.com>
2022-12-09 03:08:45 +0300

    routing init

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,18 @@
+import Link from "next/link";
+
 export const Navbar: React.FC<{ links: { path: string; title: string }[] }> = ({
   links,
 }) => {
   return (
     <nav className="p-4">
       {links.map((link) => (
-        <a key={link.toString()} href={link.path} className="text-slate-200 hover:text-slate-100 visited:text-slate-300 font-medium">
+        <Link
+          key={link.toString()}
+          href={link.path}
+          className="text-slate-200 hover:text-slate-100 visited:text-slate-300 font-medium duration-200 mr-2"
+        >
           {link.title}
-        </a>
+        </Link>
       ))}
     </nav>
   );
--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,5 +1,8 @@
 import { Navbar } from "../components/Navbar";
-const links = [{ path: "", title: "home" }];
+const links = [
+  { path: "/", title: "home" },
+  { path: "/cat", title: "cat" },
+];
 
 export const MainLayout: React.FC<{ children: any }> = ({ children }) => {
   return (
