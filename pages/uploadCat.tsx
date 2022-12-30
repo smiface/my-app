c59f06f7db408d016d0b21e3247c9277c5a58ba2
smiface <smiface.com>
2022-12-30 12:32:37 +0300

    42

--- pages/uploadCat.tsx
+++ pages/uploadCat.tsx
@@ -1,9 +1,7 @@
 // https://codesandbox.io/s/thyb0?file=/pages/api/file.js:445-457
-import axios from "axios";
 import { clsx } from "clsx";
 import { observer } from "mobx-react-lite";
-import React, { useState } from "react";
-import { CatImg } from "../components/CatImg";
+import React from "react";
 import { RootStore } from "./store/RootStore";
 
 export const textStyle = "text-slate-500 hover:text-slate-100";
@@ -15,11 +13,13 @@ const uploadCat = observer(() => {
   return (
     <>
       <input type="file" name="myImage" onChange={RootStore.cat.uploadToClient} />
+
       <img
         src={RootStore.cat.uploadCatCreateObjectURL}
         alt=""
         className="w-[240px] max-h-[300px] border-2 border-slate-200 "
       />
+
       <button
         className={clsx(customStyle, textStyle, borderStyle, " w-[240px] h-[50px] rounded-t-none border-t-0")}
         type="submit"
